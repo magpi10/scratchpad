5d764f49de37e0e4d53e2b7ef5c42b65d0406861
MagPi10 <vaughan-m@dainferncollege.co.za>
2018-06-03 07:46:00 +0200

    improved

--- python/ideas.py
+++ python/ideas.py
@@ -1,8 +1,13 @@
 import time
 from random import randint
+array = []
 for i in range(0,10):
   chose = randint(1,10)
+  array.append(chose)
   print(chose)
   chose = randint(1,10)
+  array.append(chose)
   time.sleep(1)
   print(chose)
+  print(array)
+  time.sleep(1)
