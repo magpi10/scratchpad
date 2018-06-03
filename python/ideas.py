9d29b61f403f4716f29904718a8d952345d48cc1
MagPi10 <vaughan-m@dainferncollege.co.za>
2018-06-03 10:24:15 +0200

    numbers!

--- python/ideas.py
+++ python/ideas.py
@@ -4,11 +4,9 @@ array = []
 for i in range(0,10):
   chose = randint(1,10)
   array.append(chose)
-  print(chose)
   chose = randint(1,10)
   array.append(chose)
   time.sleep(1)
-  print(chose)
   print(array)
   time.sleep(1)
   
